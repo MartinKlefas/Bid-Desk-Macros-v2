cbf64ebb0f51a5a1223f00de51c9f04cdf398014
Martin Klefas-Stennett <cleverbum@hotmail.com>
2019-05-17 16:13:37 +0100

    Search now implemented pending further tests. Missing close statements added to OPG form

--- Bid Desk Macros v2/Forms/SearchResults.vb
+++ Bid Desk Macros v2/Forms/SearchResults.vb
@@ -9,6 +9,7 @@
     Private SearchTerm As String
 
     Public Sub New(SearchTerm As String, AM As Boolean, Customer As Boolean, Deal As Boolean, OPG As Boolean, NDT As Boolean)
+        InitializeComponent()
         Me.SearchTerm = SearchTerm
         SearchIn("AM") = AM
         SearchIn("Customer") = Customer
@@ -19,5 +20,44 @@
 
     Private Sub BtnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
         Me.Close()
+    End Sub
+
+    Private Sub SearchResults_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim Columns As List(Of String) = Globals.ThisAddIn.sqlInterface.FindColumns()
+
+        For Each column In Columns
+            DataGridView1.Columns.Add(column, column)
+        Next
+
+        Dim GlobalResults As New List(Of Dictionary(Of String, String))
+
+        For Each column As String In SearchIn.Keys
+            If SearchIn(column) Then
+                Dim where As String = column & " LIKE '%" & SearchTerm & "%'"
+
+                Dim results As New List(Of Dictionary(Of String, String))
+
+                results = Globals.ThisAddIn.sqlInterface.SelectData_Dict("*", where)
+
+                If results.Count > 0 Then
+                    GlobalResults.AddRange(results)
+                End If
+            End If
+        Next
+
+        If GlobalResults.Count > 0 Then
+            DataGridView1.Rows.Add(GlobalResults.Count)
+            Dim i As Integer = 0
+            For Each tResult As Dictionary(Of String, String) In GlobalResults
+                For Each tkey In tResult.Keys
+                    DataGridView1.Item(tkey, i).Value = tResult(tkey)
+                Next
+                i += 1
+            Next
+        Else
+            DataGridView1.Rows.Add(1)
+            DataGridView1.Item(0, 0).Value = "No results found"
+        End If
     End Sub
 End Class
