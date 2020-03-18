3d36a90b514f6fe600cd5699859bca4de4a62ac2
Klefas, Martin <Martin.Klefas@insight.com>
2020-03-18 22:10:58 +0000

    fixed missing NDT number save

--- Bid Desk Macros v2/Forms/DealLog.vb
+++ Bid Desk Macros v2/Forms/DealLog.vb
@@ -179,6 +179,9 @@ Public Class AddDeal
             If newNDT <> 0 And newNDT <> 404 Then ' continue on second
 
                 If Not DealData.ContainsKey("NDT") Then DealData.Add("NDT", newNDT)
+
+                If DealData("NDT") = "" Then DealData("NDT") = newNDT
+
                 ndt.Move("Public Sector")
 
                 If Not myContinue Then
